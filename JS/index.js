512c405870eff78025a8df31c56649254eff40e5
Maptuhul Ihsan <143360641+MapIHS@users.noreply.github.com>
2023-09-12 04:08:06 +0700

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,7 @@ function onLoadLoading() {
     document.querySelector('div#loading').style.display = 'none'
     document.querySelector('div#myContent').style.display = 'block'
 }
+
+function onStart() {
+    console.log("Hello World")
+}
