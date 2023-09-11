fb0eed2505341c89b1b77898667c499091c4df56
MapIHS <maptuhul2011@gmail.com>
2023-09-11 10:52:20 +0700

    Update from up.sh

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,24 @@
+function onLoadLoading() {
+    document.querySelector('div#loading').style.display = 'none'
+    document.querySelector('div#myContent').style.display = 'block'
+}
 
+jsjs = document.querySelector("p#a")
+osksjz = document.querySelector("span#satu")
+osksjx = document.querySelector("span#dua")
+function ossks() {
+    setTimeout(()=> {
+        jsjs.innerHTML = "loading "
+        ossks()
+    }, 1400)
+    setTimeout(()=> {
+        jsjs.innerHTML += "."
+    }, 1100)
+    setTimeout(()=> {
+        jsjs.innerHTML += "."
+    }, 800)
+    setTimeout(()=> {
+        jsjs.innerHTML += "."
+    }, 500)
+}
+ossks()
